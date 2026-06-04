408a8754ff48401dee5154bad9fd4f2449f42dc7
envroworld <envro.world@gmail.com>
2026-06-04 12:17:58 +0300

    update loading state

--- assets/js/main.js
+++ assets/js/main.js
@@ -156,7 +156,7 @@ function initSW(){
     if ("serviceWorker" in navigator) {
         window.addEventListener("load", () => {
             navigator.serviceWorker
-            .register("assets/js/sw.js")
+            .register("../../sw.js")
             .then((registration) => {
                 console.log("Service Worker registered:", registration);
             })

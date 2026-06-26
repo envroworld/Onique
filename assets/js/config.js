3b1f2d41d3273b5b4cfc2d5bda83d9151b8dd32f
envroworld <envro.world@gmail.com>
2026-06-26 21:36:16 +0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
-let API_URL = "https://api.onique.space"
+let API_URL = "https://server.onique.space"
 // let API_URL = "http://127.0.0.1:8000"
 let LOCALSTORAGE_PHONE = "tx-oniqueXOX-F3INVRSE"
 let LOCALSTORAGE_DATA = "dx-oniqueXXS-F3INVERSE"
